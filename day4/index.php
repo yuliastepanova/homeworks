c4eb2f405e9cafd3cbd890cac8ff5d309967d4b1
ystsiapanava <yulia273@yandex.ru>
2017-12-01 15:55:46 +0300

    do homework for day4

---
+++ day4/index.php
@@ -0,0 +1,29 @@
+<?php
+echo exec('whoami');
+echo "hi " .$_POST['name']. "&nbsp".$_POST['surname'];
+echo "</br>";
+echo "u was born " .$_POST['date'];
+echo "</br>";
+echo "ur login is " .$_POST['login'];
+echo "</br>";
+echo "ur password " .$_POST['password'];
+echo "</br>";
+
+
+$file = $_FILES['myfile'];
+
+$dir = 'uploads';
+
+if(!file_exists($dir)){
+    mkdir($dir, 0644);
+}
+
+$file_name = $dir.'/'.$file['name'];
+echo "tmp file name ".$file_name;
+if(move_uploaded_file($file['tmp_name'], $file_name)){
+    echo "Файл успешно загружен";
+}else {
+    echo "Возникла ошибка при загрузке файла";
+}
+
+?>
